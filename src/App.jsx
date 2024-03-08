a523078129b8b9ca8c95df4da953485a0adcd4d1
Apurbwa Kumar <apurbwa@gmail.com>
2024-03-08 17:59:17 +0100

    portfolio section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-// import Test from "./Test";
 import "./app.scss";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
- 
-    <section id="Homepage">
-      <Navbar />
-      <Hero />
-    </section>
-    <section id="Services">
-      <Parallax type="Services" />
-    </section>
-    <section>
-      <Services />
-    </section>
-    <section id="Portfolio">
-      <Parallax type="Portfolio" />
-    </section>
-    <section>Portfolio</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="Services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="Portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  )
 };
 
 export default App;
